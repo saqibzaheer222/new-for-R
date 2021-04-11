3e135fbfd7a85c48571706bb008b768147600182
saqibzaheer222 <au77037@gmail.com>
2021-04-11 06:23:04 +0500

    lines added

--- learn r.R
+++ learn r.R
@@ -2,4 +2,19 @@ x=1:20
 y=21:40
 c=cbind(x,y)
 c
-sdfdf
+print('saqib zaher')
+
+x=c(1,2,3,NA,NaN)
+is.na(x)
+is.nan(x)
+
+x=data.frame(sr=1:5 , name=c('saqib','ali','qamar','haseeb','kabir'))
+x             
+nrow(x)
+x
+row.names(x)=c('a','b','c','d','e')
+x
+y=list("a","z")
+y
+y="a"
+y
